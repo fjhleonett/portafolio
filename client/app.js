880ae0b18d90801d34b50c22f4947fb3e233200c
fernando <fernandoleonett@gmail.com>
2023-04-03 20:40:19 +0200

    data

--- client/app.js
+++ client/app.js
@@ -1,5 +1,6 @@
 
 
+
 (function () {
   [...document.querySelectorAll(".control")].forEach((button) => {
     button.addEventListener("click", function () {
@@ -14,11 +15,93 @@
   });
 })();
 
+document.getElementById("presentacion").textContent = presentacion
+let habContaniner = document.getElementById("hab")
+
+skills.forEach(({ name, percent, className }) => {
+  habContaniner.innerHTML += `<div class="progress-bars">
+  
+    <div class="progress-bar">
+      <p class="prog-title">${name}</p>
+      <div class="progress-con">
+        <p class="prog-text">${percent}%</p>
+        <div class="progress">
+          <span class=${className}></span>
+        </div>
+      </div>
+    </div>
+  </div>`
+})
+
+let forContainer = document.getElementById("for")
+education.forEach(({ degree, institution, date, status }) => {
+  forContainer.innerHTML += `<div class="timeline-item">
+<div class="tl-icon">
+    <i class="fas fa-graduation-cap"></i>
+</div>
+<p class="tl-duration">${date}</p>
+<h5>${degree}<span> - ${institution}|</span></h5>
+<p>
+   ${status}
+</p>
+</div>`
+})
+let projContainer = document.getElementById("proj")
+portfolioItems.forEach(({ image, title, githubLink, liveLink }) => {
+  projContainer.innerHTML += `<div class="portfolio-item">
+  <div class="image">
+      <img src="${image}" alt="${title}">
+  </div>
+  <div class="hover-items">
+      <h3>${title} <i class="fab fa-react"></i></h3>
+      <div class="icons">
+          <a href="${githubLink}" target="_blank" class="icon">
+              <i class="fab fa-github"></i>
+          </a>
+           <a href="${liveLink}" target="_blank" class="icon">
+              <i class="fas fa-globe"></i>
+          </a>
+      </div>
+  </div>
+</div>
+
+`
+})
+
+let expContainer = document.getElementById("exp")
+experience.forEach(({ position, company, duration, description }) => {
+  expContainer.innerHTML += `<div class="timeline-item">
+  <div class="tl-icon">
+      <i class="fas fa-briefcase"></i>
+  </div>
+  <p class="tl-duration">${duration}</p>
+  <h5>${position}<span> -${company}</span></h5>
+  <p>${description}
+  </p>
+</div>
+
+`
+})
+
+
+
+// misDatos
+document.getElementById("ubicacion").textContent = misDatos.ubicacion
+document.getElementById("email").textContent = misDatos.email
+document.getElementById("tlf").textContent = misDatos.telefono
+document.getElementById("lang").textContent = misDatos.languages
+document.getElementById("linkedin").href = misDatos.linkedin
+document.getElementById("git").href = misDatos.githubLink
+
+
+
+
+
 
 const btnSubmit = document.getElementById("formContact");
 btnSubmit.addEventListener('submit', sendInformation)
 
-function sendInformation(e){
+function sendInformation(e) {
 
   e.preventDefault()
   const data = new FormData(e.target);
@@ -29,11 +112,11 @@ function sendInformation(e){
   const values = Object.fromEntries(data);
 
   try {
-   
+
     fetch("/api/contact", {
-       headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' },
       method: "POST",
-      body:JSON.stringify(values)
+      body: JSON.stringify(values)
     });
 
     Toastify({
@@ -49,7 +132,7 @@ function sendInformation(e){
     }).showToast();
 
 
-    
+
   } catch (error) {
 
     Toastify({
@@ -63,7 +146,7 @@ function sendInformation(e){
         background: "linear-gradient(#cc0d0d, #750b0b)",
       },
     }).showToast();
-    
+
 
   }
 
@@ -78,34 +161,34 @@ function sendInformation(e){
 
 
 function DownloadFile(fileName) {
-    console.log("download file");
-            //Set the File URL.
-            // var url = "Files/" + fileName;
-            var url = "./img/"+fileName;
- 
-            //Create XMLHTTP Request.
-            var req = new XMLHttpRequest();
-            req.open("GET", url, true);
-            req.responseType = "blob";
-            req.onload = function () {
-                //Convert the Byte Data to BLOB object.
-                var blob = new Blob([req.response], { type: "application/octetstream" });
- 
-                //Check the Browser type and download the File.
-                var isIE = false || !!document.documentMode;
-                if (isIE) {
-                    window.navigator.msSaveBlob(blob, fileName);
-                } else {
-                    var url = window.URL || window.webkitURL;
-                    link = url.createObjectURL(blob);
-                    var a = document.createElement("a");
-                    a.setAttribute("download", fileName);
-                    a.setAttribute("href", link);
-                    document.body.appendChild(a);
-                    a.click();
-                    document.body.removeChild(a);
-                }
-            };
-            req.send();
+  console.log("download file");
+  //Set the File URL.
+  // var url = "Files/" + fileName;
+  var url = "./img/" + fileName;
 
-          }
+  //Create XMLHTTP Request.
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.responseType = "blob";
+  req.onload = function () {
+    //Convert the Byte Data to BLOB object.
+    var blob = new Blob([req.response], { type: "application/octetstream" });
+
+    //Check the Browser type and download the File.
+    var isIE = false || !!document.documentMode;
+    if (isIE) {
+      window.navigator.msSaveBlob(blob, fileName);
+    } else {
+      var url = window.URL || window.webkitURL;
+      link = url.createObjectURL(blob);
+      var a = document.createElement("a");
+      a.setAttribute("download", fileName);
+      a.setAttribute("href", link);
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+  req.send();
+
+}
---
+++ client/datos.js
@@ -0,0 +1,118 @@
+
+const misDatos ={
+
+
+    ubicacion:"Valencia, España",
+    telefono:"(34) 670 768251",
+    email:"fernandoleonett@gmail.com",
+    githubLink:"https://github.com/FernandoLeonett",
+    linkedin:"https://www.linkedin.com/in/fernandoleonett/",
+    languages: "Español, English"
+}
+const education = [
+    {
+        degree: "Ingeniero Industrial",
+        institution: "UNEXPO Venezuela",
+        date: "2003 - 2008",
+        status: "Recibido"
+    },
+    {
+        degree: "Analista Programador",
+        institution: "Universidad Ort",
+        date: "2019 - actual",
+        status: "En curso"
+    },
+    {
+        degree: "Bootcamp Android",
+        institution: "Globant",
+        date: "2022",
+        status: "Certificado"
+    },
+    {
+        degree: "Master Diseño y Programación Web",
+        institution: "MasterMedia",
+        date: "2022 - actual",
+        status: "Cursando"
+    }
+]
+const experience = [
+    {
+      position: "Desarrollador RPA",
+      company: "Jaume y Sere",
+      duration: "2021 - 2022",
+      description: "Asistir en la robotización de los procesos administrativos y comerciales dando mantenimiento a los robots ya existentes."
+    },
+    {
+      position: "Profesor Higiene y Seguridad Industrial",
+      company: "UPTOS",
+      duration: "2014 - 2015",
+      description: "Encargado de dictar las materias de: Diseño de Sistemas de Detección y Extinción de Incendios, Seguridad en el Trabajo, Higiene en el Trabajo, Procesos Laborales, Prevención y Atención de Emergencias"
+    },
+    {
+      position: "Ejecutivo de Empresas",
+      company: "BCI, Banco Central de Inversiones",
+      duration: "2011 - 2012",
+      description: "Generar negocios atractivos y convenientes para el Banco con clientes del segmento empresarial y emprendedores, respetando las políticas comerciales y el marco ético de las operaciones bancarias."
+    },
+    {
+      position: "Ejecutivo de Seguros",
+      company: "Falabella Seguros",
+      duration: "2009 - 2011",
+      description: "Ofrecer el producto que mejor se adapta a la realidad del cliente y a los objetivos comerciales de la empresa, teniendo en cuenta las debilidades de la competencia y convirtiéndolas en oportunidades."
+    },
+    {
+      position: "Consultor Six Sigma",
+      company: "BANESCO Banco Universal",
+      duration: "2008 - 2009",
+      description: "Responsable de implementar mejoras de procesos en las sucursales de la región metropolitana de Caracas en base a la metodología Six Sigma."
+    },
+    {
+      position: "RTC",
+      company: "Michelin",
+      duration: "2009-2010",
+      description: "Representante técnico comercial de Michelin Venezuela para el área metropolitana de Caracas y atención al cliente carga pesada."
+    }
+  ];
+
+
+  const portfolioItems = [
+    {
+      image: "img/port2.jpg",
+      title: "Ecommerce - Next Js",
+      githubLink: "https://github.com/FernandoLeonett/simpleshop",
+      liveLink: "https://gregory-shop-comida.vercel.app/"
+    },
+    {
+      image: "img/port3.jpg",
+      title: "RESERVA PARKING",
+      subtitle: "<i class='fab fa-android'></i>",
+      githubLink: "https://github.com/FernandoLeonett/AndrodidBootCampFer"
+    },
+    {
+      image: "img/port4.jpg",
+      title: "Full Project - C#",
+      githubLink: "https://github.com/FernandoLeonett/Obligatorio2P3"
+    }
+  ];
+  const skills = [
+    { name: "HTML 5", percent: 90, className: "html" },
+    { name: "JavaScript-TypeScript", percent: 95, className: "javascript" },
+    { name: "Node JS", percent: 85, className: "js" },
+    { name: "React Hooks JS", percent: 90, className: "react" },
+    { name: "NodeJS", percent: 87, className: "node" },
+    { name: "C#", percent: 90, className: "C" },
+    { name: "Java", percent: 80, className: "java" },
+    { name: "SQL Server", percent: 90, className: "sql" },
+    { name: "Firebase", percent: 85, className: "firebase" },
+    { name: "Supabase", percent: 85, className: "supabase" },
+    { name: "Chakra UI", percent: 80, className: "chakra" },
+    { name: "UI Path", percent: 80, className: "uipath" },
+    { name: "Git", percent: 90, className: "git" },
+    { name: "GitHub", percent: 90, className: "github" }
+  ];
+  
+
+
+  const presentacion = "¡Hola! Soy un programador perseverante y apasionado por las soluciones eficientes y escalables. Tengo experiencia en Node.js, TypeScript, React, C# y Java, y estoy dispuesto a aprender nuevas tecnologías. Además, cuento con formación académica en ingeniería industrial y análista progrmador. Me enfoco en seguir buenas prácticas de programación y crear soluciones de calidad para mis proyectos. ¡Estoy emocionado por ser parte de nuevos desafíos!"
+  
+  
